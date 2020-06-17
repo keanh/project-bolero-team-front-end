ebbfacd00a2006f77b9b5792de96d2108a323b3e
hien <hien07072001@gmail.com>
2020-06-17 15:08:38 +0700

    giao dien thanh cong

---
+++ src/app/user/play-music/play-music.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-play-music',
+  templateUrl: './play-music.component.html',
+  styleUrls: ['./play-music.component.css']
+})
+export class PlayMusicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,16 +4,21 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout/layout.component';
 import {RouterModule, Routes} from '@angular/router';
+import { PlayMusicComponent } from './play-music/play-music.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent
+    component: LayoutComponent,
+    children: [
+      {path: '',
+      component: PlayMusicComponent}
+    ]
   }
 ];
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent, LayoutComponent],
+  declarations: [FooterComponent, HeaderComponent, LayoutComponent, PlayMusicComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
