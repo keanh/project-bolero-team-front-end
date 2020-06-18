2634e7a23d2591255d7930233ea7c55394829ace
xuantho <hoxuantho95@gmail.com>
2020-06-18 14:43:51 +0700

    fix conflix merge xuantho - keanh

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -7,7 +7,10 @@ import {RouterModule, Routes} from '@angular/router';
 import { PlayMusicComponent } from './play-music/play-music.component';
 import { AddSongComponent } from './add-song/add-song.component';
 import {StyleListComponent} from './style-list/style-list.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from '@angular/forms';
+// import { MVComponent } from './mv/mv.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+import {CKEditorModule} from 'ckeditor4-angular';
 
 const routes: Routes = [
   {
@@ -18,6 +21,10 @@ const routes: Routes = [
         path: '',
       component: PlayMusicComponent
       },
+      // {
+      //   path: '',
+      //   component: MVComponent
+      // },
       {
         path: 'add',
         component: AddSongComponent
@@ -31,11 +38,17 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent, LayoutComponent, PlayMusicComponent, AddSongComponent, StyleListComponent],
+  declarations: [FooterComponent,
+    HeaderComponent,
+    LayoutComponent,
+    PlayMusicComponent,
+    AddSongComponent,
+    StyleListComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CKEditorModule
   ]
 })
-export class UserModule { }
+export class UserModule {}
