8dcfd8496abce2a55ecb6cc37d5249531fb51992
MrEverything <nampp1204@gmail.com>
2020-06-25 11:51:32 +0700

    sua create song

--- src/app/song/add-song/add-song.component.ts
+++ src/app/song/add-song/add-song.component.ts
@@ -51,7 +51,7 @@ export class AddSongComponent implements OnInit {
       this.getUserDetail();
     }
     this.songForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(1)]],
+      name: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(100)]],
       lyrics: ['', [Validators.required, Validators.minLength(10)]],
       singer: ['', [Validators.required, Validators.minLength(1)]],
       author: ['', [Validators.required, Validators.minLength(1)]],
@@ -143,7 +143,7 @@ export class AddSongComponent implements OnInit {
         console.log(e);
       });
       console.log(song);
-      await this.router.navigate(['song/list']);
+      await this.router.navigate(['/list']);
       this.createSuccess();
       this.songForm.reset();
       // alert('create thành công');
@@ -152,13 +152,13 @@ export class AddSongComponent implements OnInit {
   createSuccess(){
     this.Toast.fire({
       icon: 'success',
-      title: 'Tạo mới thành công'
+      title: ' create success '
     });
   }
   createFail(){
     this.Toast.fire({
-      icon: 'success',
-      title: 'Tạo mới that bai'
+      icon: 'error',
+      title: 'create fail'
     });
   }
 
@@ -179,5 +179,7 @@ export class AddSongComponent implements OnInit {
     }, error =>
       console.log(error));
   }
+
+
 }
 
