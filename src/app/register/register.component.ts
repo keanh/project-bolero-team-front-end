74481f22721c8cdbe7ec68c3f1f97f77281368c9
hien <hien07072001@gmail.com>
2020-06-25 08:35:27 +0700

    form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -17,9 +17,11 @@ export class RegisterComponent implements OnInit {
   isSignUpFailed = false;
   errorMessage = '';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {
+  }
 
-  ngOnInit() { }
+  ngOnInit() {
+  }
 
   onSubmit() {
     console.log(this.form);
@@ -46,77 +48,4 @@ export class RegisterComponent implements OnInit {
       }
     );
   }
-//   registerForm: FormGroup;
-//   submitted = false;
-//   phoneNumberPattern = '^((\\0-?)|0)?[0-9]{10}$';
-//   // user: User[] = [];
-//   failMessage: string;
-//   Toast = Swal.mixin({
-//     toast: true,
-//     position: 'top-end',
-//     showConfirmButton: false,
-//     timer: 3000
-//   });
-//   constructor(private formBuilder: FormBuilder,
-//               private userService: UserService,
-//               private authService: AuthService
-//   ) {}
-//   ngOnInit() {
-//     this.registerForm = this.formBuilder.group({
-//       firstName: ['', Validators.required],
-//       lastName: ['', Validators.required],
-//       username: ['', Validators.required],
-//       email: ['', [Validators.required, Validators.email]],
-//       phoneNumber: ['', [Validators.required, Validators.pattern(this.phoneNumberPattern)]],
-//       password: ['', [Validators.required, Validators.minLength(6)]],
-//       // confirmPassword: ['', Validators.required],
-//       acceptTerms: [false, Validators.requiredTrue]
-//     }, {
-//       validator: MustMatch('password', 'confirmPassword')
-//     });
-//   }
-//   // convenience getter for easy access to form fields
-//   get f() { return this.registerForm.controls; }
-//
-//   onSubmit() {
-//     this.submitted = true;
-//     // stop here if form is invalid
-//     if (this.registerForm.valid) {
-//       const {value} = this.registerForm;
-//       this.authService.signUp(value)
-//         .subscribe(() => {
-//           this.createSuccess();
-//           this.registerForm.reset(this.onReset()
-//           );
-//         }, error => {
-//           this.failMessage = 'Add user fail !';
-//         });
-//     }
-//   }
-//   createSuccess(){
-//     this.Toast.fire({
-//       icon: 'success',
-//       title: 'Tạo mới thành công'
-//     });
-//   }
-//   onReset() {
-//     this.submitted = false;
-//     this.registerForm.reset();
-//   }
-// }
-// export function MustMatch(controlName: string, matchingControlName: string) {
-//   return (formGroup: FormGroup) => {
-//     const control = formGroup.controls[controlName];
-//     const matchingControl = formGroup.controls[matchingControlName];
-//     if (matchingControl.errors && !matchingControl.errors.mustMatch) {
-//       // return if another validator has already found an error on the matchingControl
-//       return;
-//     }
-//     // set error on matchingControl if validation fails
-//     if (control.value !== matchingControl.value) {
-//       matchingControl.setErrors({ mustMatch: true });
-//     } else {
-//       matchingControl.setErrors(null);
-//     }
-//   };
 }
