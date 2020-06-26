b28cd8f0c1deae47b1830d3767c4b37a3a3e92b8
keanh <keanh275@gmail.com>
2020-06-26 08:09:51 +0700

    commit dau ngay

--- src/app/song/all-song/all-song.component.ts
+++ src/app/song/all-song/all-song.component.ts
@@ -25,16 +25,15 @@ export class AllSongComponent implements OnInit, OnChanges {
   ) {
   }
   ngOnInit(): void {
+    this.getUserInfor();
+    if (this.info.username !== ''){
+      this.getUserDetail();
+    }
+    this.getListSearch();
     this.songService.getAllSongs().subscribe(next => {
       this.songList = next;
     }, error => (this.songList = []));
-    this.getListSearch();
-    // this.songService.getAllSongsLatest().subscribe(next => {
-    // }, error => (this = []));
-    // this.getUserInfor();
-    // this.likeService.getLikes().subscribe(next => {
-    //   this.likeList = next;
-    // }, error => (this.likeList = []));
+    this.getUserInfor();
   }
   getListSearch(){
     this.searchService.list.subscribe( data => {
@@ -50,11 +49,9 @@ export class AllSongComponent implements OnInit, OnChanges {
     this.info = {
       token: this.tokenService.getToken(),
       username: this.tokenService.getUsername(),
-      // authorities: this.token.getAuthorities()
     };
-    // console.log(this.info);
-    // this.accessToken = this.token.getToken();
   }
+
   getUserDetail(){
     this.userService.getUserByUserName(this.info.username).subscribe( data =>
     {
@@ -62,20 +59,29 @@ export class AllSongComponent implements OnInit, OnChanges {
     }, error =>
       console.log(error));
   }
-  // onLike( idUser: number, idSong: number) {
-  //   console.log(idUser);
-  //   const like = {
-  //     user: idUser
-  //   };
-  //   console.log(like);
-  //   like.user = this.convertToUser(idUser);
-  //   this.songService.likeSong(like, idSong).subscribe(next => {
-  //     this.getAllLastSong();
-  //     this.getAllSong();
-  //     console.log(idSong);
-  //     console.log(next);
-  //   }, (e) => {
-  //     console.log(e);
-  //   });
-  // }
+
+  onLike( idUser: number, idSong: number) {
+    const like = {
+      user: idUser
+    };
+    like.user = this.convertToUser(idUser);
+    this.songService.likeSong(like, idSong).subscribe(next => {
+      this.getAllSong();
+    }, (e) => {
+      console.log(e);
+    });
+  }
+
+  getAllSong(){
+    this.songService.getAllSongs().subscribe(next => {
+      this.songList = next;
+    }, error => (this.songList = []));
+  }
+
+  convertToUser( idUser: number){
+    const user: any = {
+      id: idUser
+    };
+    return user;
+  }
 }
